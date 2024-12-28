0cff60bddd39c4e6b095f7fc86441923136e0987
INEEDNZT <554214611@qq.com>
2024-12-29 04:01:56 +0800

    download & install model when compile

---
+++ utils/download-ulfd-model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+declare -A networks=(
+    # tflite RFB version model
+    ["https://github.com/Linzaer/Ultra-Light-Fast-Generic-Face-Detector-1MB/blob/master/tflite/pretrained/version-RFB-320_without_postprocessing.tflite"]="version-RFB-320_without_postprocessing.tflite"
+)
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+dir=$1
+mkdir -p $dir
+
+for url in "${!networks[@]}"; do
+    filename="${networks[$url]}"
+    wget -nv -O "$dir/$filename" "$url"
+done
